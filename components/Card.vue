ded20635d1aa699b3c15937ba32dbab7c4e43766
jmwhitworth <jackmartinwhit@gmail.com>
2024-11-11 17:27:38 +0000

    Added new image component to handle async image src

--- components/Card.vue
+++ components/Card.vue
@@ -3,18 +3,18 @@
     class="space-y-4 rounded-lg border-2 border-gray-200/20 bg-gray-950 p-4 pb-6 text-gray-100 transition-colors duration-200 hover:border-gray-200/30"
   >
     <figure class="relative w-full overflow-hidden rounded-t-md pb-[56.25%]">
-      <img
+      <Image
         :src="image_url"
         :alt="image_alt"
         class="absolute bottom-0 left-0 right-0 top-[-16.75%] w-full object-center"
-        loading="eager"
-      />
+      ></Image>
     </figure>
     <slot></slot>
   </div>
 </template>
 
 <script>
+import Image from './Image.vue'
 export default {
   props: {
     image_url: {
---
+++ components/Image.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, defineProps, nextTick, watch } from 'vue'
+import { onMounted } from 'vue'
+
+const imageSrc = ref('')
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+})
+
+onMounted(async () => {
+  await nextTick()
+  imageSrc.value = props.src
+})
+
+watch(
+  () => props.src,
+  async (newSrc) => {
+    await nextTick()
+    imageSrc.value = newSrc
+  },
+)
+</script>
+
+<template>
+  <img :src="imageSrc" :alt="alt" />
+</template>
